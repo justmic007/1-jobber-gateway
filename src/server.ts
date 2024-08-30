8f540ed0d594e1ae62ad3943ad347c9c85e101c8
Micah J <justmic007>
2024-08-30 21:37:36 +0100

    setup gateway server

---
+++ src/server.ts
@@ -0,0 +1,104 @@
+import http from 'http';
+
+import { CustomError, IErrorResponse, winstonLogger } from '@justmic007/jobber-shared';
+import cookieSession from 'cookie-session';
+import { Application, json, NextFunction, Request, Response, urlencoded } from 'express';
+import { Logger } from 'winston';
+import cors from 'cors';
+import hpp from 'hpp';
+import helmet from 'helmet';
+import compression from 'compression';
+import { StatusCodes } from 'http-status-codes';
+
+import { config } from './config';
+
+const SERVER_PORT = 4000;
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
+
+export class GatewayServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware();
+    this.startElasticsearch();
+    this.errorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.set('trust proxy', 1);
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [`${config.SECRET_KEY_ONE}`, `${config.SECRET_KEY_TWO}`],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development' // update with value from config. For production environment, set value to TRUE to force usage of https protocol
+        // sameSite: none
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(cors({
+      origin: config.CLIENT_URL,
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    }));
+  }
+
+  // A middleware to reduce the size of the json request
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '200mb'}));
+    app.use(urlencoded({ extended: true, limit: '200mb'}));
+  }
+
+  private routesMiddleware(): void {
+
+  }
+
+  private startElasticsearch(): void {
+
+  }
+
+  private errorHandler(app: Application): void {
+    app.use('*', (req: Request, res: Response, next: NextFunction) => {
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      log.log('error', `${fullUrl} endpoint does not exist.`, '');
+      res.status(StatusCodes.NOT_FOUND).json({ message: 'The endpoint called does not exist.'});
+      next();
+    });
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.log('error', `GatewayService ${error.comingFrom}:` , error);
+      if (error instanceof CustomError) {
+        res.status(error.statusCode).json(error.serializedErrors());
+      }
+      next();
+    });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
+    }
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+      log.info(`Gateway server has started with process id ${process.pid}`);
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`Gateway server running on port ${SERVER_PORT}`);
+      });
+    } catch (error) {
+      log.log('error', 'GatewayService startServer() error method:', error);
+    }
+  }
+}
